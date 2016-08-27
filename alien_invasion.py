06725fa5a39026a3cb87dc3c84b0cd0f5fb0bc03
jhldreams <504354161@qq.com>
2016-08-27 10:05:47 +0800

    settings.py and ship.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
-import sys
 import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -12,20 +13,19 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # 创建一艘飞船
+
+    ship = Ship(screen)
+
     # 开始游戏的主循环
 
     while True:
-
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
-        # 背景色填充
-        screen.fill(ai_settings.bg_color)
-
+        # 重绘屏幕,背景色填充
         # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
--- ship.py
+++ ship.py
@@ -4,18 +4,30 @@ import pygame
 class Ship():
     def __init__(self, screen):
         """初始化飞船并设置其初始位置"""
+
+        # 此处screen是父窗口传递过来的surface对象
         self.screen = screen
 
         # 加载飞船图像并获取其外接矩形
 
+        # 将不规则图形按照矩形处理,rect是Ship对象的一个内部变量,用来代替image的矩形
+        # screen_rect
         self.image = pygame.image.load('images/ship.bmp')
-        self.rect = self.image
+        self.rect = self.image.get_rect()
+        # 拿到父级窗口的的rect属性
         self.screen_rect = screen.get_rect()
 
         # 将每艘新飞船放在屏幕底部中央
-        self.rect.centerx = self.screen_rect.centerx
+
+        # 屏幕中央,注意这些值是坐标
+        self.rect.center = self.screen_rect.center
+
+        print(self.rect.center)
+        # 屏幕底部
         self.rect.bottom = self.screen_rect.bottom
 
     def blitme(self):
         """在指定位置绘制飞船"""
+
+        # blit():draw one image onto another
         self.screen.blit(self.image, self.rect)
