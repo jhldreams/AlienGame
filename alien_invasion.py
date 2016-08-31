80b8b72539ce8ba5a8b5e8ceb59e76b09f8609c1
jhldreams <504354161@qq.com>
2016-08-31 10:24:56 +0800

    frank, alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,7 +8,7 @@ def run_game():
     # 初始化游戏并创建一个屏幕对象
 
     pygame.init()
-    ai_settings = Settings()
+    ai_settings = Settings()  # 屏幕大小和背景色的设置
 
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
